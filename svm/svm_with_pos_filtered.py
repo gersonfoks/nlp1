1dcdbab8bd2d06db66915dec4f5f68ef22b0500c
Gerson <gerson.foks@gmail.com>
2020-11-13 11:38:38 +0100

    added svm to notebook

--- svm/svm_with_pos_filtered.py
+++ svm/svm_with_pos_filtered.py
@@ -1,13 +1,9 @@
 from svm.utils import *
 from sklearn import svm
 
-
 reviews = load_reviews()
 
-
-
 X, Y = prepare_dataset(reviews, review_to_words_pos_filtered)
-
 
 voc = create_voc_of_features(X)
 
@@ -15,18 +11,8 @@ index_mapping = create_index_mapping(voc)
 
 X = create_feature_matrix(X, index_mapping)
 
-
 fold_indexes = create_fold_indexes(reviews, n_folds=10)
 
-
-result_dict = train_on_folds_svm(svm.SVC, X,Y, fold_indexes)
+result_dict = train_on_folds_svm(svm.SVC, X, Y, fold_indexes)
 print(result_dict['predictions'])
 print(result_dict['accuracies'])
-
-
-
-
-
-
-
-
